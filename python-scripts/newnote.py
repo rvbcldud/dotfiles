8291624f7ec46bce4b676a8a9332fe2267ec64e0
rvbcldud <ryanlee6767@gmail.com>
2021-02-15 17:18:29 -0800

    Added ability to create new note

--- python-scripts/newnote.py
+++ python-scripts/newnote.py
@@ -1,8 +1,19 @@
+# Import libraries
 import os
+import datetime
 
+# Gets the path to the notes folder
 path = os.environ.get('NOTES_DIR')
 
-print(path)
+time = datetime.datetime.now()
+
+note_name = time.strftime("%Y-%m-%d_%H:%M") + "_Note.md"
+
+new_note = open(note_name, "x")
+new_note.write("# Notes for " + time.strftime("%Y-%m-%d at %H:%M")+ "\n")
+new_note.close()
+
+os.system(('vim ' + note_name))
 
 # TODO: CREATE A NEW MARKDOWN FILE WITH THE
 #       DATE AT THE BEGINNING
